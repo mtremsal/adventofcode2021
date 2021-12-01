2a061728c6c0318e9f6c29015673fdf84a38c1d5
Marc Tremsal <marc.tremsal@gmail.com>
2021-12-01 18:41:34 -0500

    Refactor to prepare for puzzle 01-b

--- test/adventofcode2021_test.exs
+++ test/adventofcode2021_test.exs
@@ -2,14 +2,14 @@ defmodule Adventofcode2021Test do
   use ExUnit.Case
   doctest Adventofcode2021
 
-  test "day 01 sample" do
-    sample = Adventofcode2021.read_sample("01")
+  test "day 01-a sample" do
+    sample = Adventofcode2021.read_sample("01-a")
     assert Adventofcode2021.Day01.solve(sample) == 7
   end
 
-  test "day 01 input" do
-    input = Adventofcode2021.read_input("01")
-    output = Adventofcode2021.Day01.solve(input) |> Adventofcode2021.write_output("01")
+  test "day 01-a input" do
+    input = Adventofcode2021.read_input("01-a")
+    output = Adventofcode2021.Day01.solve(input) |> Adventofcode2021.write_output("01-a")
     assert output == :ok
   end
 end
