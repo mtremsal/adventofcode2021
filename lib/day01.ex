e9d68b4d4271666e9ebb0c0dba9a093b64acae5c
Marc Tremsal <marc.tremsal@gmail.com>
2021-12-01 18:53:53 -0500

    Solve for the sample of puzzle 01-b

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,10 +1,38 @@
 defmodule Adventofcode2021.Day01 do
   require Logger
 
-  def solve(file) do
-    Logger.debug("Solving for #{__MODULE__}")
+  def solve_a(file) do
+    Logger.debug("Solving problem a in #{__MODULE__}")
     array = file |> String.split("\n")
     count_increases(array, -1, 0) # The -1 avoids incrementing the count on the first measurement
+  end
+
+  def solve_b(file) do
+    Logger.debug("Solving problem b in #{__MODULE__}")
+    array = file |> String.split("\n")
+    count_increases_by_three(array, 0, {nil, nil, nil})
+  end
+
+  defp count_increases_by_three([], total, _measures) do
+    total
+  end
+
+  defp count_increases_by_three([""], total, _measures) do
+    total
+  end
+
+  defp count_increases_by_three(file, total, {a, b, nil}) do
+    [head | tail] = file
+    count_increases_by_three(tail, total, {String.to_integer(head), a, b})
+  end
+
+  defp count_increases_by_three(file, total, {a, b, c}) do
+    [head | tail] = file
+    if String.to_integer(head) + a + b > a + b + c do
+      count_increases_by_three(tail, total + 1, {String.to_integer(head), a, b})
+    else
+      count_increases_by_three(tail, total, {String.to_integer(head), a, b})
+    end
   end
 
   defp count_increases([], total, _latest_depth) do
--- test/adventofcode2021_test.exs
+++ test/adventofcode2021_test.exs
@@ -4,12 +4,17 @@ defmodule Adventofcode2021Test do
 
   test "day 01-a sample" do
     sample = Adventofcode2021.read_sample("01-a")
-    assert Adventofcode2021.Day01.solve(sample) == 7
+    assert Adventofcode2021.Day01.solve_a(sample) == 7
   end
 
   test "day 01-a input" do
     input = Adventofcode2021.read_input("01-a")
-    output = Adventofcode2021.Day01.solve(input) |> Adventofcode2021.write_output("01-a")
+    output = Adventofcode2021.Day01.solve_a(input) |> Adventofcode2021.write_output("01-a")
     assert output == :ok
+  end
+
+  test "day 01-b sample" do
+    sample = Adventofcode2021.read_sample("01-b")
+    assert Adventofcode2021.Day01.solve_b(sample) == 5
   end
 end
