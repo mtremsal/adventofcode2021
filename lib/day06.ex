96260152373b49b0c6f5de0bd478b6ac3c190e56
Marc Tremsal <marc.tremsal@gmail.com>
2021-12-06 10:47:50 -0500

    Optimize and submit puzzle Day06-a

--- lib/day06.ex
+++ lib/day06.ex
@@ -10,20 +10,17 @@ defmodule Adventofcode2021.Day06 do
       |> String.trim("\n")
       |> String.split(",", trim: true)
       |> Enum.map(&String.to_integer/1)
-      |> Enum.map(fn age -> {age, 6} end)
 
     1..80
     |> Enum.reduce(init_school, fn _gen, school -> age_the_school(school) end)
     |> length
   end
 
-  # def age_a_fish({0, max_age}), do: [{max_age, max_age}, {max_age + 2, max_age + 2}]
-  def age_a_fish({0, 6}), do: [{6, 6}, {8, 8}]
-  def age_a_fish({0, 8}), do: [{6, 6}, {8, 8}]
-  def age_a_fish({age, max_age}), do: [{age - 1, max_age}]
+  def age_a_fish(0), do: [6, 8]
+  def age_a_fish(age), do: [age - 1]
 
   def age_the_school(school) do
     Logger.debug("School length: #{inspect(length(school))}")
-    school |> Enum.reduce([], fn fish, acc -> acc ++ age_a_fish(fish) end)
+    school |> Enum.reduce([], fn fish, acc -> age_a_fish(fish) ++ acc end)
   end
 end
